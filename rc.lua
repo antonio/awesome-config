1c43cfc2b37762c54a8931eff91cedb5d3aae635
Antonio Santos <antonio@santosvelasco.com>
2012-07-22 16:56:44 +0200

    Center SSH askpass

--- rc.lua
+++ rc.lua
@@ -294,15 +294,20 @@ root.keys(globalkeys)
 -- {{{ Rules
 awful.rules.rules = {
   -- All clients will match this rule.
-  { rule = { },
-  properties = {
-    border_width = beautiful.border_width,
-    border_color = beautiful.border_normal,
-    focus = true,
-    keys = clientkeys,
-    buttons = clientbuttons
-  }
-},
+  {
+    rule = { class = "Gcr-prompter" },
+    properties = { floating = true, ontop = true },
+    callback = awful.placement.centered
+  },
+  {
+    rule = { },
+    properties = {
+      border_width = beautiful.border_width,
+      border_color = beautiful.border_normal,
+      focus = true,
+      keys = clientkeys,
+      buttons = clientbuttons }
+  },
 }
 -- }}}
 
