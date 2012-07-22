e8b4343fd43bfbbe083263ea7af9fa0b89ef2103
Antonio Santos <antonio@santosvelasco.com>
2012-07-22 17:02:07 +0200

    Show battery status

--- rc.lua
+++ rc.lua
@@ -225,10 +225,11 @@ clientkeys = awful.util.table.join(
   awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
   awful.key({ modkey, "Shift"   }, "r",      function (c) c:redraw()                       end),
   awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end),
-  awful.key({ modkey,           }, "m", function (c)
-                                          c.maximized_horizontal = not c.maximized_horizontal
-                                          c.maximized_vertical   = not c.maximized_vertical
-                                        end))
+  awful.key({ modkey,           }, "b",      function() awful.util.spawn("battery")        end),
+  awful.key({ modkey,           }, "m",      function (c)
+                                               c.maximized_horizontal = not c.maximized_horizontal
+                                               c.maximized_vertical   = not c.maximized_vertical
+                                             end))
 
 -- Compute the maximum number of digit we need, limited to 9
 keynumber = 0
