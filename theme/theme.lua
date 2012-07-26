82c7c570e4c5c82e19f7dbb0568e311b0586bd36
Antonio Santos <antonio@santosvelasco.com>
2012-07-26 16:04:44 +0200

    Change theme paths and background

--- theme/theme.lua
+++ theme/theme.lua
@@ -1,3 +1,4 @@
+local awful = require('awful')
 theme = {}
 
 theme.font = "Ubuntu Sans Mono 8"
@@ -33,14 +34,14 @@ theme.taglist_bg_urgent = "#373737"
 --theme.taglist_bg_focus = "#ff0000"
 
 -- Display the taglist squares
-theme.taglist_squares_sel   = "/usr/share/awesome/themes/default/taglist/squarefw.png"
-theme.taglist_squares_unsel = "/usr/share/awesome/themes/default/taglist/squarew.png"
-theme.tasklist_floating_icon = "/usr/share/awesome/themes/default/tasklist/floatingw.png"
+theme.taglist_squares_sel   = awful.util.getdir("config") .. "/theme/taglist/squarefw.png"
+theme.taglist_squares_unsel = awful.util.getdir("config") .. "/theme/taglist/squarew.png"
+theme.tasklist_floating_icon = awful.util.getdir("config") .. "/theme/tasklist/floatingw.png"
 
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = "/usr/share/awesome/themes/default/submenu.png"
+theme.menu_submenu_icon = awful.util.getdir("config") .. "/theme/submenu.png"
 theme.menu_height = "15"
 theme.menu_width  = "100"
 
@@ -50,45 +51,45 @@ theme.menu_width  = "100"
 --theme.bg_widget = "#cc0000"
 
 -- Define the image to load
-theme.titlebar_close_button_normal = "/usr/share/awesome/themes/default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/default/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = awful.util.getdir("config") .. "/theme/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = awful.util.getdir("config") .. "/theme/titlebar/close_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = awful.util.getdir("config") .. "/theme/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = awful.util.getdir("config") .. "/theme/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = awful.util.getdir("config") .. "/theme/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = awful.util.getdir("config") .. "/theme/titlebar/ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = awful.util.getdir("config") .. "/theme/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = awful.util.getdir("config") .. "/theme/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = awful.util.getdir("config") .. "/theme/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = awful.util.getdir("config") .. "/theme/titlebar/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = awful.util.getdir("config") .. "/theme/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = awful.util.getdir("config") .. "/theme/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = awful.util.getdir("config") .. "/theme/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = awful.util.getdir("config") .. "/theme/titlebar/floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = awful.util.getdir("config") .. "/theme/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = awful.util.getdir("config") .. "/theme/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = awful.util.getdir("config") .. "/theme/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = awful.util.getdir("config") .. "/theme/titlebar/maximized_focus_active.png"
 
 -- You can use your own command to set your wallpaper
-theme.wallpaper_cmd = { "awsetbg /usr/share/awesome/themes/default/background.png" }
+theme.wallpaper_cmd = { "feh --bg-center " .. awful.util.getdir("config") .. "/theme/background.png" }
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
-theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
-theme.layout_floating  = "/usr/share/awesome/themes/default/layouts/floatingw.png"
-theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifierw.png"
-theme.layout_max = "/usr/share/awesome/themes/default/layouts/maxw.png"
-theme.layout_fullscreen = "/usr/share/awesome/themes/default/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/usr/share/awesome/themes/default/layouts/tileleftw.png"
-theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
-theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
-theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
-theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
+theme.layout_fairh = awful.util.getdir("config") .. "/theme/layouts/fairhw.png"
+theme.layout_fairv = awful.util.getdir("config") .. "/theme/layouts/fairvw.png"
+theme.layout_floating  = awful.util.getdir("config") .. "/theme/layouts/floatingw.png"
+theme.layout_magnifier = awful.util.getdir("config") .. "/theme/layouts/magnifierw.png"
+theme.layout_max = awful.util.getdir("config") .. "/theme/layouts/maxw.png"
+theme.layout_fullscreen = awful.util.getdir("config") .. "/theme/layouts/fullscreenw.png"
+theme.layout_tilebottom = awful.util.getdir("config") .. "/theme/layouts/tilebottomw.png"
+theme.layout_tileleft   = awful.util.getdir("config") .. "/theme/layouts/tileleftw.png"
+theme.layout_tile = awful.util.getdir("config") .. "/theme/layouts/tilew.png"
+theme.layout_tiletop = awful.util.getdir("config") .. "/theme/layouts/tiletopw.png"
+theme.layout_spiral  = awful.util.getdir("config") .. "/theme/layouts/spiralw.png"
+theme.layout_dwindle = awful.util.getdir("config") .. "/theme/layouts/dwindlew.png"
 
 theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
 
