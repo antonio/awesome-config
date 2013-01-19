5639b550f7cc7bc1a3c1435f23877d4e7eff1221
Antonio Santos <antonio@santosvelasco.com>
2013-01-19 14:54:51 +0100

    Simple pomodoro widget

--- rc.lua
+++ rc.lua
@@ -198,10 +198,15 @@ for s = 1, screen.count() do
     left_layout:add(mytaglist[s])
     left_layout:add(mypromptbox[s])
 
+    require "pomodoro"
+
+    pomodoro_widget = pomodoro.init()
+
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
-    if s == 1 then right_layout:add(wibox.widget.systray()) end
+    right_layout:add(pomodoro_widget.widget)
     right_layout:add(mytextclock)
+    if s == 1 then right_layout:add(wibox.widget.systray()) end
     right_layout:add(mylayoutbox[s])
 
     -- Now bring it all together (with the tasklist in the middle)
